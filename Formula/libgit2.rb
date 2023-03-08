04e62d1c807bc4de977098b6c22f758c0c6dae30
Ryan Bigg <me@ryanbigg.com>
2023-03-09 10:50:01 +1100

    libgit2 1.6.2

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -1,8 +1,8 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v1.5.2.tar.gz"
-  sha256 "57638ac0e319078f56a7e17570be754515e5b1276d3750904b4214c92e8fa196"
+  url "https://github.com/libgit2/libgit2/archive/v1.6.2.tar.gz"
+  sha256 "d557fbf35557bb5df53cbf38ae0081edb4a36494ec0d19741fa673e509245f8f"
   license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
   head "https://github.com/libgit2/libgit2.git", branch: "main"
 
